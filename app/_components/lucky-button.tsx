ea1fb72f00abd7db8d571d84199330ec36b67565
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-12 22:49:00 -0300

    feat: add LuckyButton component

---
+++ app/_components/lucky-button.tsx
@@ -0,0 +1,16 @@
+type LuckyButtonProps = {
+    handleLuckyBtnClick: () => void;
+};
+
+const LuckyButton = ({ handleLuckyBtnClick }: LuckyButtonProps) => {
+    return (
+        <button
+            onClick={handleLuckyBtnClick}
+            className="cursor-pointer rounded bg-green-500 px-4 py-2 text-white"
+        >
+            I&apos;m Feeling Lucky
+        </button>
+    );
+};
+
+export default LuckyButton;
