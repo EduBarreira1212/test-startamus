b00c3c7499ff2db0624c82cfe3062989d07e4f98
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-13 20:09:23 -0300

    feat: update LuckyJokeCard to display joke categories and improve structure

--- app/_components/lucky-joke-card.tsx
+++ app/_components/lucky-joke-card.tsx
@@ -1,12 +1,26 @@
+import { Joke } from '../page';
+
 type LuckyJokeCardProps = {
-    luckyJoke: string;
+    luckyJoke: Joke;
 };
 
 const LuckyJokeCard = ({ luckyJoke }: LuckyJokeCardProps) => {
     return (
-        <div className="rounded border bg-white p-4 shadow">
-            <p className="text-black">{luckyJoke}</p>
-        </div>
+        <article className="max-w-[80%] rounded border bg-white p-4 shadow">
+            <p className="text-black">{luckyJoke.value}</p>
+            {luckyJoke.categories.length > 0 && (
+                <div className="mt-2 flex gap-2">
+                    {luckyJoke.categories.map((category) => (
+                        <span
+                            key={category}
+                            className="rounded bg-gray-200 px-2 py-1 text-gray-800"
+                        >
+                            Category: {category}
+                        </span>
+                    ))}
+                </div>
+            )}
+        </article>
     );
 };
 
