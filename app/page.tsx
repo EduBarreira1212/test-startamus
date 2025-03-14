d2e8a0e228da8566624569fea5659e8334fcdc29
Eduardo Barreira <edubarreira1212@gmail.com>
2025-03-13 21:26:18 -0300

    feat: add loading state and spinner to Home component for better user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import LuckyButton from './_components/lucky-button';
 import SearchJoke from './_components/search-joke';
 import LuckyJokeCard from './_components/lucky-joke-card';
 import JokesCard from './_components/jokes-card';
 import { fetchJokes } from './_actions/fetch-jokes';
 import { fetchRandomJoke } from './_actions/fetch-random-joke';
+import Image from 'next/image';
 
 export type Joke = {
     categories: string[];
@@ -21,6 +22,7 @@ export type Joke = {
 export default function Home() {
     const [query, setQuery] = useState('');
     const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [luckyJoke, setLuckyJoke] = useState<Joke | null>(null);
     const [jokes, setJokes] = useState<Joke[]>([]);
 
@@ -28,48 +30,61 @@ export default function Home() {
         e.preventDefault();
         if (!query.trim()) return;
 
+        setLoading(true);
         const data = await fetchJokes(query);
         setLuckyJoke(null);
         if (data.length === 0) {
             setError(true);
             setJokes([]);
+            setLoading(false);
             return;
         }
         setJokes(data);
         setError(false);
+        setLoading(false);
     };
 
     const handleLuckyBtnClick = async () => {
+        setLoading(true);
         setJokes([]);
         setError(false);
         const data = await fetchRandomJoke();
         setLuckyJoke(data);
+        setLoading(false);
     };
 
     return (
-        <main className="flex h-screen w-full flex-col items-center gap-4 p-3">
+        <main className="flex h-screen w-full flex-col items-center gap-5 p-3">
             <h1 className="text-3xl">Piadas do Chuck Norris</h1>
-            <SearchJoke
-                handleSubmit={handleSubmit}
-                query={query}
-                setQuery={setQuery}
-            />
-            <LuckyButton handleLuckyBtnClick={handleLuckyBtnClick} />
+            <section className="flex w-full justify-center gap-4">
+                <SearchJoke
+                    handleSubmit={handleSubmit}
+                    query={query}
+                    setQuery={setQuery}
+                />
+                <LuckyButton handleLuckyBtnClick={handleLuckyBtnClick} />
+            </section>
 
             {luckyJoke && <LuckyJokeCard luckyJoke={luckyJoke} />}
 
+            {loading && (
+                <Image
+                    src="/loading.svg"
+                    alt="loading"
+                    width={50}
+                    height={50}
+                    className="mt-5 animate-spin"
+                />
+            )}
+
             <section className="flex w-full flex-col items-center gap-4 overflow-auto">
-                <Suspense
-                    fallback={<p role="alert text-white">Carregando piadas...</p>}
-                >
-                    <ul className="flex w-full flex-col items-center gap-4">
-                        {jokes.map((joke) => (
-                            <li key={joke.id} className="flex w-full justify-center">
-                                <JokesCard joke={joke} query={query} />
-                            </li>
-                        ))}
-                    </ul>
-                </Suspense>
+                <ul className="flex w-full flex-col items-center gap-4">
+                    {jokes.map((joke) => (
+                        <li key={joke.id} className="flex w-full justify-center">
+                            <JokesCard joke={joke} query={query} />
+                        </li>
+                    ))}
+                </ul>
             </section>
 
             {error && <p role="alert">Nenhuma piada encontrada.</p>}
